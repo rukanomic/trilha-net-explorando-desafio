1984c5471cbd85183e21259d8188843ef015efa8
Ruka <lucas_brito8@hotmail.com>
2025-01-18 22:31:45 -0300

    desafios resolvidos

--- Program.cs
+++ Program.cs
@@ -6,17 +6,21 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Ruka");
+Pessoa p2 = new Pessoa(nome: "Lpeh");
+Pessoa p3 = new Pessoa(nome: "Gust");
+Pessoa p4 = new Pessoa(nome: "Sumy");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 100);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
